c93650bc17e779f1ed8c9f54086502aadc876fcf
1501299026 <sinumsd98@gmail.com>
2021-05-13 06:56:46 -0700

    ADDED UC1

--- employeewagebuilder/EmployeeWage.java
+++ employeewagebuilder/EmployeeWage.java
@@ -4,6 +4,14 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee");
+		Random rand = new Random();
+		int attendance=rand.nextInt(2);
+		if (attendance==0)
+			System.out.println("Employee is Absent");
+		else
+			System.out.println("Employee is Present");
+	}
 
 	}
 
